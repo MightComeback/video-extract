f703cb0c14e42966a73182f6ff8f4ce46a0f34cb
Ivan Kuznetsov <kuznetsovivan496@gmail.com>
2026-01-31 12:51:48 +0200

    feat(youtube): add isYoutubeUrl and extractYoutubeId helpers

---
+++ test/youtube-detector.test.js
@@ -0,0 +1,15 @@
+import { test } from 'node:test';
+import assert from 'node:assert';
+import { isYoutubeUrl, extractYoutubeId } from '../src/youtube.js';
+
+test('isYoutubeUrl identifies valid YouTube URLs', () => {
+  assert.strictEqual(isYoutubeUrl('https://www.youtube.com/watch?v=dQw4w9WgXcQ'), true);
+  assert.strictEqual(isYoutubeUrl('https://youtu.be/dQw4w9WgXcQ'), true);
+  assert.strictEqual(isYoutubeUrl('https://youtube.com/embed/dQw4w9WgXcQ'), true);
+  assert.strictEqual(isYoutubeUrl('https://www.youtube.com/shorts/dQw4w9WgXcQ'), true);
+});
+
+test('extractYoutubeId extracts ID correctly', () => {
+  assert.strictEqual(extractYoutubeId('https://www.youtube.com/watch?v=dQw4w9WgXcQ'), 'dQw4w9WgXcQ');
+  assert.strictEqual(extractYoutubeId('https://youtu.be/dQw4w9WgXcQ'), 'dQw4w9WgXcQ');
+});
